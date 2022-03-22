7da3c2b7578850c2693e074845df81ca85cbc98f
P.Lane <kc9eye@users.noreply.github.com>
2022-03-22 17:55:57 -0500

    Update xml2json.php

--- xml2json.php
+++ xml2json.php
@@ -4,4 +4,9 @@ echo
 '{
   "GalnetArchive":{
     "version":'.$archive->version.',
-    
+    "articles":[
+    '
+foreach($archive->article as $article) {
+  echo '{"id":"'.$article->id.'","link":"'.$article->link.'","title":"'.$article->title.'","date":"'.$article->date.'","story":"'.htmlspecialchars($article->story,ENT_QUOTES,'UTF-8').'"},';
+}
+echo ']}';
